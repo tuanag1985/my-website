6c9d00a8ec2c79a90cee8b0b21765add358e2f2c
PHAM ANH TUAN <vuontoimotngoisao85@gmail.com>
2026-01-04 20:09:05 +0700

    Fix case sensitivity for Public folder

--- Server.js
+++ Server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000; //Dòng này có nghĩa là: "Sử dụng cổng của nhà cung cấp, nếu không có thì mới dùng cổng 3000".
 
 // khai báo thư mục public để server gọi chạy các file trong thư mục
-app.use(express.static('public')); //dòng này chạy file index trước
+app.use(express.static('Public')); //dòng này chạy file index trước
 
 // route trang chủ
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));// dòng này chạy file muốn chỉ định nhưng phải đưa lên trước dòng express.static
+    res.sendFile(path.join(__dirname, 'Public', 'index.html'));// dòng này chạy file muốn chỉ định nhưng phải đưa lên trước dòng express.static
 });
 
 app.listen(PORT, () => {
